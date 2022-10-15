a263df7a10ca171c461f3aaa59a34e01cd903420
Danika <danikadonis@gmail.com>
2022-10-15 15:35:03 -0600

    Mostrar.php

--- view/pruebaimage/mostrar.php
+++ view/pruebaimage/mostrar.php
@@ -4,35 +4,15 @@
 <title>Mostrar Imagenes</title>
 </head>
 <body>
-<center>
-<table border="2">
-<thead>
-    <tr> 
-        <th colspan="5"><a href="index.php">Nuevo</a></th>
-    </tr>
-<tr>
-<th>id</th>
-<th>Nombre</th>
-<th>Imagen</th>
-<th colspan="2">Operaciones</th>
-</tr>
-</thead>
-<tbody>
+
 <?php
 include("conexion.php");
-Squery= "SELECT * FROM images"; $resultado $conexion->query($query); while($row = $resultado->fetch_assoc()){
+$query= "SELECT * FROM tabla_imagen"; 
+$resultado = $conexion->query($query); 
+$row = $resultado->fetch_assoc();
 ?>
-<tr>
-<td><?php echo $row['id']; ?></td>
-<td><?php echo $row['user_id']; ?></td>
-<td><img height="70px" src="data:image/png;base64,<?php echo base64_encode($row['image']); ?>"/></td>
-<th><a href="#">Modificar</a></th>
-<th><a href="#">Eliminar</a></th>
-</tr>
+<img height="70px" src="data:image/jpg;base64,<?php echo base64_encode($row['Imagen']); ?>"/>
 <?php
-}
 ?>
-</table>
-</center>
 </body>
 </html>
