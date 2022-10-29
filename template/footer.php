7838a063bc25d54930bcaef7d2a030a51e8b2fdf
“Londy” <londyperez1502@gmail.com>
2022-10-29 10:53:42 -0700

    Prueba Footer

--- template/footer.php
+++ template/footer.php
@@ -1,14 +1,12 @@
 <footer class="py-4 my-3 footerCustom">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Opción 1</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Opción 2</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Opción 3</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Opción 4</a></li>
-<<<<<<< HEAD
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contacto 1</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contacto 2</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contacto 3</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contacto 4</a></li>
 </ul>
 =======
     </ul>
->>>>>>> 0b063f9 (pequeños ajustes)
     <p class="text-center text-muted">&copy; Derechos de Autor y temas de leyes</p>
 </footer>
 </body>
