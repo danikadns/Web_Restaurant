1ba8798800767be2381f5c3d9a6425997f9fc1c1
“Londy” <londyperez1502@gmail.com>
2022-11-24 10:54:13 -0800

    view de alimentos

---
+++ view/Alimentos/alimentosView.php
@@ -0,0 +1,152 @@
+<?php
+session_start();
+if (!$_SESSION['user_id']) {
+    header("location: ../../index.php");
+}
+
+include_once("../../model/functions.php");
+
+$usrClass = new usuariosModel();
+
+$result = array();
+$resultRoles = array();
+$result = $usrClass->getAlimentos();
+
+?>
+<script src="assets/js/moduloAlimentos.js"></script>
+<div class="card">
+    <div class="card-header">
+        <div
+            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">LISTADO DE ALIMENTOS</h1>
+        </div>
+    </div>
+
+    <div class="card-body">
+
+
+        <div class="container">
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button class="btn btn-success me-md-2" id="btnNuevoAlimento" name="btnNuevoAlimento" type="button"
+                    data-bs-toggle="modal" data-bs-target="#formNuevoAlimento">Nuevo Alimento</button>
+            </div>
+            <div class="table-responsive">
+                <table class="table ">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">NOMBRE</th>
+            
+                            <th scope="col">ESTADO</th>
+                            
+                            <th scope="col">EDITAR</th>
+                            <th scope="col">ELIMINAR</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <?php
+                while ($fila = mysqli_fetch_array($result)) {
+                    ?>
+                        <tr>
+                            <th><?php echo $fila['id']; ?></th>
+                            <td><?php echo $fila['nombres']; ?></td>
+                            
+                            <td><?php echo $fila['estado']; ?></td>
+                            
+                            <td>
+                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                    <button class="btn btn-warning me-md-2" id="btnEditarALimento"
+                                        name="btnEditarAlimento" type="button"
+                                        onclick="obtenerAlimento(<?php echo $fila['id']; ?>);"> Editar</button>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                    <button class="btn btn-danger me-md-2" id="btnEliminarAlimento"
+                                        onclick="eliminarAlimento(<?php echo $fila['id']; ?>);" name="btnEliminarAlimento"
+                                        type="button">Eliminar</button>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <?php
+                }
+?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- AQUI INICIA ESTA EL FORMULARIO MODAL PARA AGREGAR ALIMENTOS -->
+    <div class="modal fade " id="formNuevoAlimento" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h5 class="modal-title" id="formNuevoAlimento">Nuevo Alimento</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="nombres" placeholder="aqui va tu nombre">
+                        <label for="nombres">Nombres</label>
+                    </div>
+
+                  
+
+
+
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" id="btnAgregarAlimento">Agregar Usuario</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- AQUI ESTA EL FORMULARIO MODAL PARA ACTUALIZAR ALIMENTOS -->
+    <div class="modal fade" id="formActualizaAlimento" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h5 class="modal-title" id="formActualizaAlimento">Usuario</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="id_upd">
+                        <label for="id_upd">ID</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="nombres_upd" placeholder="aqui va el nombre del alimento">
+                        <label for="nombres_upd">Nombres</label>
+                    </div>
+
+                  
+
+
+
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" id="btnActualizarAliemnto">Actualizar
+                            Usuario</button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
