30e1073fc19302a6955c6984760ff3259383ef9f
Danika <danikadonis@gmail.com>
2022-11-25 19:35:35 -0600

    orden cocinero 1.1

---
+++ assets/js/moduloOrden.js
@@ -0,0 +1,42 @@
+function DespacharOrden(id) {
+
+    Swal.fire({
+        title: '¿Estas seguro?',
+        text: "!No podras revertir este cambio!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: 'POST',
+                data: "act_estado_orden=1&id=" + id,
+                url: 'controller/Orden/ordenController.php',
+                dataType: 'json',
+
+                success: function (data) {
+                    var resultado = data.resultado;
+
+                    if (resultado === 1) {
+
+                        Swal.fire(
+                            '!Eliminado!',
+                            'Borrado de la base de datos',
+                            'success'
+                        )
+                        cargarContenido('view/OrdenCocina/OrdenCocinaView.php');
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: '!Algo salió mal!',
+                        })
+                    }
+                }
+
+            })
+        }
+    })
+}
